8192b36ab72830975b296d4d78d7b185ad175ad6
iver gentz <ivergentz@gmail.com>
2020-10-19 14:16:38 +0200

    added globalstyles to project

---
+++ src/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 112.5%;
+    font-weight: 100;
+}
+`
