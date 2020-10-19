0d7b5749ed12757ca82899c35400f8cdc9c6f292
iver gentz <ivergentz@gmail.com>
2020-10-19 14:30:57 +0200

    Added moment and datastructure

---
+++ data/CardData.js
@@ -0,0 +1,39 @@
+import moment from 'moment'
+
+export default [
+  {
+    title: 'Test Title 1',
+    subTitle: 'Test undertitle 1',
+    image: '1.jpg',
+    smallImage: '1.jpg',
+    date: moment().format('DD. MMMM YYYY'),
+  },
+  {
+    title: 'Test Title 2',
+    subTitle: 'Test undertitle 2',
+    image: '2.jpg',
+    smallImage: '2.jpg',
+    date: moment().format('DD. MMMM YYYY'),
+  },
+  {
+    title: 'Test Title 3',
+    subTitle: 'Test undertitle 3',
+    image: '3.jpg',
+    smallImage: '3.jpg',
+    date: moment().format('DD. MMMM YYYY'),
+  },
+  {
+    title: 'Test Title 4',
+    subTitle: 'Test undertitle 4',
+    image: '4.jpg',
+    smallImage: '4.jpg',
+    date: moment().format('DD. MMMM YYYY'),
+  },
+  {
+    title: 'Test Title 5',
+    subTitle: 'Test undertitle 5',
+    image: '5.jpg',
+    smallImage: '5.jpg',
+    date: moment().format('DD. MMMM YYYY'),
+  },
+]
